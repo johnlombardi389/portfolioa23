d548a80fdee6a3917cf4ce4325dac4279d0516da
johnlombardi389 <lombardij38@gmail.com>
2023-04-12 14:55:19 -0400

    Set up grid layout in portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,31 @@ import { motion } from "framer-motion";
 const Portfolio = () => {
   return (
     <div>
-      <h1>Hello Portfolio</h1>
+      <GridContainer>
+        <GridItem>Test</GridItem>
+        <GridItem>Test</GridItem>
+        <GridItem>Test</GridItem>
+        <GridItem>Test</GridItem>
+        <GridItem>Test</GridItem>
+        <GridItem>Test</GridItem>
+      </GridContainer>
     </div>
   );
 };
+
+const GridContainer = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 1rem;
+  margin: 1rem;
+`;
+
+const GridItem = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  background-color: blue;
+`;
 
 export default Portfolio;
