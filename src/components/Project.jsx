a47a20b8b92fb839c650b4e43329bed064dba426
johnlombardi389 <lombardij38@gmail.com>
2023-04-15 12:10:12 -0400

    Styled buttons for project details with color and box shadows

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -26,10 +26,10 @@ const Project = ({ selectedData }) => {
               <h3>{project.title}</h3>
               <p>{project.description}</p>
               <Buttons>
-                <a href={project.demo} target="_blank">
+                <a href={project.demo} target="_blank" className="black-btn">
                   Live Demo
                 </a>
-                <a href={project.github} target="_blank">
+                <a href={project.github} target="_blank" className="white-btn">
                   GitHub
                 </a>
               </Buttons>
@@ -47,7 +47,7 @@ const ProjectView = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: green;
+  background-color: white;
   margin: 0 auto;
 
   img {
@@ -66,19 +66,30 @@ const Buttons = styled(motion.div)`
   width: 15rem;
   margin: 2rem auto;
 
+  .black-btn {
+    background-color: black;
+  }
+
+  .white-btn {
+    background-color: white;
+  }
+
   a {
     display: inline-block;
     margin: 0.5rem 0;
     padding: 10px 20px;
     width: 100%;
     background-color: #4caf50;
-    color: white;
+    color: blue;
     text-align: center;
     text-decoration: none;
     font-size: 16px;
     border-radius: 4px;
     border: none;
     cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em,
+      rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
+      rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
   }
 `;
 
