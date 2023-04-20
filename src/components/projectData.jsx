f4736920e38b1f2ef2cf7fef439f15f01dc941e9
johnlombardi389 <lombardij38@gmail.com>
2023-04-20 11:30:33 -0400

    Removed images and added video files to data with updated project details

--- src/components/projectData.jsx
+++ src/components/projectData.jsx
@@ -1,151 +1,78 @@
-// Images
-import layoutGen from "../assets/layoutGen.png";
-import musicPlay from "../assets/musicPlay.png";
-import pixelSketch from "../assets/pixelSketch.png";
-import rockPaper from "../assets/rockPaper.png";
-import signUp from "../assets/signUp.png";
-import travelApp from "../assets/travelApp.png";
-
 // Videos
-import pumpkinvideo from "../assets/pumpkinvideo.mp4";
-
-// Icons
-import {
-  SiReact,
-  SiJavascript,
-  SiSass,
-  SiCss3,
-  SiHtml5,
-  SiVite,
-  SiVisualstudiocode,
-  SiFigma,
-  SiGit,
-} from "react-icons/si";
+import gamePacVid from "../assets/gamePacVid.mp4";
+import layoutGenVid from "../assets/layoutGenVid.mp4";
+import musicPlayerVid from "../assets/musicPlayerVid.mp4";
+import pixelSketchVid from "../assets/pixelSketchVid.mp4";
+import portVid from "../assets/portVid.mp4";
+import rpsVid from "../assets/rpsVid.mp4";
 
 const projectData = () => [
   {
     id: 1,
-    title: "Layout Generator",
+    title: "Game Pac",
     description:
-      "PGenerate layouts for your website with this generator you can use in your browser.",
-    tools: ["React", "JavaScript", "CSS", "HTML"],
+      "A gaming website that allows users to discover and learn about upcoming games, view popular titles, and search for specific games of interest.",
+    tools: ["React", "Redux", "JavaScript", "Sass"],
     color: "pink",
-    image: layoutGen,
-    demo: "https://johnlombardi389.github.io/layout-generator/",
-    github: "https://github.com/johnlombardi389/layout-generator",
-    toolkit: ["JavaScript", "CSS", "HTML", "Git", "Figma"],
-    toolkitIcons: [
-      <SiJavascript />,
-      <SiCss3 />,
-      <SiHtml5 />,
-      <SiGit />,
-      <SiFigma />,
-    ],
-    video: pumpkinvideo,
+    demo: "https://johnlombardi389.github.io/game-tracker/",
+    github: "https://github.com/johnlombardi389/game-tracker",
+    video: gamePacVid,
   },
   {
     id: 2,
-    title: "Sign Up",
-    description: "This a game to play Rock, Paper, Scissors on your computer",
-    tools: ["Python", "Node", "Express", "HTML"],
+    title: "Music Player",
+    description:
+      "Browse and play songs from the library, with the ability to skip forward and back.",
+    tools: ["React", "JavaScript", "Sass", "Git"],
     color: "blue",
-    image: signUp,
     demo: "https://johnlombardi389.github.io/music-player/",
     github: "https://github.com/johnlombardi389/music-player",
-    toolkit: ["React", "JavaScript", "Sass", "CSS", "HTML", "Git", "Figma"],
-    toolkitIcons: [
-      <SiReact />,
-      <SiJavascript />,
-      <SiSass />,
-      <SiCss3 />,
-      <SiHtml5 />,
-      <SiGit />,
-      <SiFigma />,
-    ],
-    video: pumpkinvideo,
+    video: musicPlayerVid,
   },
   {
     id: 3,
-    title: "Music Player",
-    description: "This a game to play Rock, Paper, Scissors on your computer",
-    tools: ["Figma", "Flask", "Express", "HTML"],
+    title: "Pixel Sketch",
+    description:
+      "A web-based application inspired by the classic etch-a-sketch toy. The app allows users to create drawings on a digital canvas by manipulating pixels.",
+    tools: ["JavaScript", "CSS", "HTML", "Figma"],
     color: "yellow",
-    image: musicPlay,
-    demo: "https://johnlombardi389.github.io/music-player/",
-    github: "https://github.com/johnlombardi389/music-player",
-    toolkit: ["React", "JavaScript", "Vite", "CSS", "HTML", "Git", "VS Code"],
-    toolkitIcons: [
-      <SiReact />,
-      <SiJavascript />,
-      <SiVite />,
-      <SiCss3 />,
-      <SiHtml5 />,
-      <SiGit />,
-      <SiVisualstudiocode />,
-    ],
-    video: pumpkinvideo,
+    demo: "https://johnlombardi389.github.io/pixel-sketch/",
+    github: "https://github.com/johnlombardi389/pixel-sketch",
+    video: pixelSketchVid,
   },
   {
     id: 4,
-    title: "Travel App",
-    description: "This a game to play Rock, Paper, Scissors on your computer",
-    tools: ["Photoshop", "Git", "GitHub", "VS Code"],
+    title: "Layout Generator",
+    description:
+      "A tool that both developers and designers can use for inspiration in creating websites.",
+    tools: ["JavaScript", "CSS", "HTML", "Git"],
     color: "blue",
-    image: travelApp,
-    demo: "https://johnlombardi389.github.io/music-player/",
-    github: "https://github.com/johnlombardi389/music-player",
-    toolkit: ["Vite", "JavaScript", "Sass", "CSS", "HTML", "Git", "Figma"],
-    toolkitIcons: [
-      <SiVite />,
-      <SiJavascript />,
-      <SiSass />,
-      <SiCss3 />,
-      <SiHtml5 />,
-      <SiGit />,
-      <SiFigma />,
-    ],
-    video: pumpkinvideo,
+    demo: "https://johnlombardi389.github.io/layout-generator/",
+    github: "https://github.com/johnlombardi389/layout-generator",
+    video: layoutGenVid,
   },
   {
     id: 5,
-    title: "Pixel Sketch",
+    title: "Portfolio Website",
     description:
       "Pixel Sketch is a web-based application inspired by the classic etch-a-sketch toy. The app allows users to create drawings on a digital canvas by manipulating pixels, similar to the way one would use a physical etch-a-sketch. The app is built using a combination of technologies, including vanilla JavaScript, CSS, and HTML. The project was managed and version controlled using Git, and the design of the app was created using Figma.",
     descriptionTwo:
       "The main feature of Pixel Sketch is its customizable canvas, which allows users to adjust the size of the canvas and the color of the pixels. Additionally, the app includes a toggle function that allows users to turn on and off a grid overlay on the canvas, giving them more precise control over their drawings. The app is designed with a simple and intuitive user interface, making it easy for anyone to start creating their own digital etch-a-sketches.",
-    tools: ["JavaScript", "SASS", "CSS", "HTML"],
+    tools: ["React", "JavaScript", "Sass", "Vite"],
     color: "yellow",
-    image: pixelSketch,
-    demo: "https://johnlombardi389.github.io/pixel-sketch/",
-    github: "https://github.com/johnlombardi389/pixel-sketch",
-    toolkit: ["JavaScript", "CSS", "HTML", "Git", "Figma"],
-    toolkitIcons: [
-      <SiJavascript />,
-      <SiCss3 />,
-      <SiHtml5 />,
-      <SiGit />,
-      <SiFigma />,
-    ],
-    video: pumpkinvideo,
+    demo: "https://johnlombardi389.github.io/portfolio/",
+    github: "https://github.com/johnlombardi389/portfolio",
+    video: portVid,
   },
   {
     id: 6,
     title: "RPS Game",
     description: "This a game to play Rock, Paper, Scissors on your computer",
-    tools: ["JavaScript", "Illustrator", "Figma", "HTML"],
+    tools: ["JavaScript", "CSS", "HTML", "Git"],
     color: "pink",
-    image: rockPaper,
-    demo: "https://johnlombardi389.github.io/music-player/",
-    github: "https://github.com/johnlombardi389/music-player",
-    toolkit: ["React", "JavaScript", "Sass", "Git", "Figma"],
-    toolkitIcons: [
-      <SiReact />,
-      <SiJavascript />,
-      <SiSass />,
-      <SiGit />,
-      <SiFigma />,
-    ],
-    video: pumpkinvideo,
+    demo: "https://johnlombardi389.github.io/rock-paper-scissors/",
+    github: "https://github.com/johnlombardi389/rock-paper-scissors",
+    video: rpsVid,
   },
 ];
 
